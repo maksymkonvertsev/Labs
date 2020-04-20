4297af636f0d8f6a0e2ec21b3492ea4d19237341
konwersevvv <konwersevvv@gmail.com>
2020-04-20 23:33:00 +0300

    1

---
+++ WebFormsLab3/AppCode/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsLab3.AppCode
+{
+    public class ProductKMA
+    {
+        public string Name { get; set; }
+        public int PlanCost { get; set; }
+        public int ReportCost { get; set; }
+        public double PlanComplitionPercentage
+        {
+            get
+            {
+                return ReportCost * 100 / PlanCost;
+            }
+            
+        }
+    }
+}
